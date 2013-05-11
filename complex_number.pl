ec03802ca116c5d8f6ff7b432dc7b6a9fcc7de0d
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2013-05-11 09:15:19 +0200

    complex numers are now more "lazy"

--- complex_number.pl
+++ complex_number.pl
@@ -6,55 +6,47 @@
                            cartesian_vector/2
                           ]).
 
-normalize_term(Z, complex(R, 0)) :-
+:- use_module(utils, [safe_is/2]).
+
+normalize_term(Z, complex(R1, I1)) :-
+    normalize_term_(Z, complex(R0, I0)),
+    safe_is(R1, R0),
+    safe_is(I1, I0).
+
+normalize_term_(Z, complex(Z, 0)) :-
     \+ compound(Z),
-    Z \= i,
-    (   ground(Z) -> R is Z ; R = Z ).
+    Z \= i.
 
-normalize_term(A * B, complex(R, I)) :-
+normalize_term_(A * B, complex(AR * BR, AI * BI)) :-
+    binary_expr(A, B, complex(AR, AI), complex(BR, BI)),
+    !.
+
+normalize_term_(A + B, complex(AR + BR, AI + BI)) :-
+    binary_expr(A, B, complex(AR, AI), complex(BR, BI)),
+    !.
+
+normalize_term_(A - B, complex(AR - BR, AI - BI)) :-
+    binary_expr(A, B, complex(AR, AI), complex(BR, BI)),
+    !.
+
+normalize_term_(i, complex(0, 1)).
+
+normalize_term_(R + I * i, complex(R, I)).
+normalize_term_(R - I * i, complex(R, -I)).
+
+normalize_term_(i * I, complex(0, I)).
+normalize_term_(I * i, complex(0, I)).
+
+normalize_term_(-Z, complex(-R, -I)) :-
+    normalize_term_(Z, complex(R, I)).
+
+normalize_term_(complex(R, I), complex(R, I)).
+
+binary_expr(A, B, AZ, BZ) :-
     compound(A),
     compound(B),
-    normalize_term(A, complex(AR, AI)),
-    normalize_term(B, complex(BR, BI)),
-    !,
-    R is AR * BR,
-    I is AI * BI.
-
-normalize_term(A + B, complex(R, I)) :-
-    compound(A),
-    compound(B),
-    normalize_term(A, complex(AR, AI)),
-    normalize_term(B, complex(BR, BI)),
-    !,
-    R is AR + BR,
-    I is AI + BI.
-
-normalize_term(A - B, complex(R, I)) :-
-    compound(A),
-    compound(B),
-    normalize_term(A, complex(AR, AI)),
-    normalize_term(B, complex(BR, BI)),
-    !,
-    R is AR - BR,
-    I is AI - BI.
-
-normalize_term(i, complex(0, 1)).
-
-normalize_term(R0 + I0 * i, complex(R1, I1)) :-
-    R1 is R0,
-    I1 is I0.
-normalize_term(R0 - I0 * i, complex(R1, I1)) :-
-    R1 is R0,
-    I1 is -I0.
-normalize_term(i * A, complex(0, I)) :- I is A.
-normalize_term(A * i, complex(0, I)) :- I is A.
-
-normalize_term(-Z, complex(R1, I1)) :-
-    normalize_term(Z, complex(R0, I0)),
-    R1 is -R0,
-    I1 is -I0.
-
-normalize_term(complex(R, I), complex(R, I)).
+    normalize_term_(A, AZ),
+    normalize_term_(B, BZ).
 
 real(Z, R)  :- normalize_term(Z, complex(R, _I)).
 imaginary(Z, I) :- normalize_term(Z, complex(_R, I)).
@@ -96,6 +88,11 @@ test('normalize_term(A + B)') :-
 
 test('normalize_term(A * B)') :-
     normalize_term(complex(2, 3) * complex(4, 5), complex(8, 15)).
+
+test('normalize_term(R - I * i, complex(4, -3))') :-
+    normalize_term(R - I * i, complex(4, -3)),
+    R is 4,
+    I is 3.
 
 test(real) :- real(complex(pi, 0), pi).
 
--- utils.pl
+++ utils.pl
@@ -1,4 +1,8 @@
-:- module(utils, [ground_semidet/2]).
+:- module(utils, [
+                  ground_semidet/2,
+                  safe_is/2,
+                  numeric_inverse/2
+                 ]).
 :- meta_predicate ground_semidet(?,1).
 
 ground_semidet(Var, Goal) :-
@@ -6,3 +10,15 @@ ground_semidet(Var, Goal) :-
     ->  call(Goal, Var), !
     ;   call(Goal, Var)
     ).
+
+safe_is(A, B) :-
+    (   A == B -> A = B
+    ;   ground(B) -> A is B
+    ;   ground(A), var(B) -> B is A
+    ;   numeric_inverse(A, B)
+    ).
+
+numeric_inverse(A, -B) :-
+    number(A),
+    var(B),
+    B is -A.
