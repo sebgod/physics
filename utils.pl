d9ccf5fd9b78d02c0bbd224716365e4568539fa6
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2013-05-11 10:38:29 +0200

    added tests for safe_is

--- utils.pl
+++ utils.pl
@@ -11,14 +11,42 @@ ground_semidet(Var, Goal) :-
     ;   call(Goal, Var)
     ).
 
+%%	safe_is(+A, +B) is semidet.
+%%	safe_is(-A, +B) is det.
 safe_is(A, B) :-
-    (   A == B -> A = B
-    ;   ground(B) -> A is B
-    ;   ground(A), var(B) -> B is A
+    (   A == B                -> A = B, _ is A
+    ;   ground(B), var(A)     -> A is B
+    ;   ground(A), var(B)     -> B is A
+    ;   ground(A), ground(B)  -> A =:= B
     ;   numeric_inverse(A, B)
     ).
 
-numeric_inverse(A, -B) :-
+numeric_inverse(-A, B) :-
+    numeric_inverse_(B, -A).
+    %var(A),
+    %number(B),
+    %A is -B.
+
+numeric_inverse_(A, -B) :-
     number(A),
     var(B),
     B is -A.
+
+
+:- begin_tests(utils).
+
+test('safe_is(0,1)', [fail]) :- safe_is(0, 1).
+test('safe_is(A, 1)') :- safe_is(A, 1), A == 1.
+test('safe_is(pi, pi)') :- safe_is(pi, pi).
+test('safe_is(A, pi)', [setup(Pi is pi)]) :- safe_is(Pi, pi).
+test('safe_is(A, hello)',
+     [throws(error(type_error(evaluable,hello/0),
+                   context(system:is/2,_))
+            )]
+    ) :-
+    safe_is(_, hello).
+test('safe_is(1 =:= sin(pi/2))') :- safe_is(1, sin(pi/2)).
+test('safe_is(-A, -3)', [A =:= 3]) :- safe_is(-A, -3).
+test('safe_is(-3, -A)', [A =:= 3]) :- safe_is(-3, -A).
+
+:- end_tests(utils).
